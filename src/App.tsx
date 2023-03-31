6f2edcaf8207ee82e911317bc75af224690e45f2
hyojchaB <hyojcha@smilegate.com>
2023-03-31 20:31:30 +0900

    chore: styled-component install

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return <div className="App">hello world</div>;
+interface Text {
+  text: string;
 }
+
+const Box = styled.div<Text>`
+  width: 200px;
+  height 200px;
+  border: 1px solid red;
+  ${({ theme }) => theme.media.desktop`        
+      border: 2px solid blue;
+      ${(props: Text) => `&::before{
+          content:"데스크톱 ${props.text}"
+      }`};
+  `}
+  ${({ theme }) => theme.media.tablet`
+      border: 2px solid yellow;
+      ${(props: Text) => `&::before{
+          content:"태블릿 ${props.text}"
+      }`}
+  `}
+  ${({ theme }) => theme.media.mobile`
+      border: 2px solid purple;
+      ${(props: Text) => `&::before{
+          content:"모바일 ${props.text}"
+      }`}
+  `}
+`;
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Box text="사이즈입니다" />
+    </div>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import GlobalStyle from './styles/global-style';
+import theme from './styles/theme';
+import { ThemeProvider } from './styles/theme-components';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
